26c18cdfa7cff8901c59e20908c6e6ec74946d2d
URK23CS1209 <kondojusharon@karunya.edu.in>
2025-11-03 16:52:54 +0530

    Update

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api', // ✅ full backend URL
+  baseURL: 'https://crimetrack-mern-app.onrender.com/api', // ✅ full backend URL
 });
 
 
